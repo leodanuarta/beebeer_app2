7c01b141c41357dd5cf9ba3501980325e3425867
Hendrick Hendrick <hendrick.535210004@stu.untar.ac.id>
2023-06-03 01:35:05 +0700

    Home view UI 2:37:35

---
+++ lib/features/home/view/home_view.dart
@@ -0,0 +1,35 @@
+import 'package:beebeer_app2/constants/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeView extends StatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const HomeView(),
+      );
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final appBar = UIConstants.appBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      bottomNavigationBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.homeFilledIcon,
+            ),
+          ),
+        ],
+      ), //CupertinoTabBar
+    );
+  }
+}
