e8f494241b193bb495dc5037247f2bc103f7bd69
Hendrick Hendrick <hendrick.535210004@stu.untar.ac.id>
2023-06-07 16:05:55 +0700

    update tweet_controller.dart & providers.dart & storage_api.dart 3:57:57

--- lib/features/tweet/controller/tweet_controller.dart
+++ lib/features/tweet/controller/tweet_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:beebeer_app2/apis/storage_api.dart';
 import 'package:beebeer_app2/apis/tweet_api.dart';
 import 'package:beebeer_app2/core/enums/tweet_type_enum.dart';
 import 'package:beebeer_app2/core/utils.dart';
@@ -13,18 +14,22 @@ final tweetControllerProvider = StateNotifierProvider<TweetController, bool>(
     return TweetController(
       ref: ref,
       tweetAPI: ref.watch(tweetAPIProvider),
+      storageAPI: ref.watch(storageAPIProvider),
     );
   },
 );
 
 class TweetController extends StateNotifier<bool> {
   final TweetAPI _tweetAPI;
+  final StorageAPI _storageAPI;
   final Ref _ref;
   TweetController({
     required Ref ref,
     required TweetAPI tweetAPI,
+    required StorageAPI storageAPI,
   })  : _ref = ref,
         _tweetAPI = tweetAPI,
+        _storageAPI = storageAPI,
         super(false);
 
   void shareTweet({
@@ -55,7 +60,29 @@ class TweetController extends StateNotifier<bool> {
     required List<File> images,
     required String text,
     required BuildContext context,
-  }) {}
+  }) async {
+    state = true;
+    final hashtags = _getHashtagsFromText(text);
+    String link = _getLinkFromText(text);
+    final user = _ref.read(currentUserDetailsProvider).value!;
+    final imageLinks = await _storageAPI.uploadImage(images);
+    Tweet tweet = Tweet(
+      text: text,
+      hashtags: hashtags,
+      link: link,
+      imageLinks: imageLinks,
+      uid: user.uid,
+      tweetType: TweetType.image,
+      tweetedAt: DateTime.now(),
+      likes: const [],
+      commentIds: const [],
+      id: '',
+      reshareCount: 0,
+    );
+    final res = await _tweetAPI.shareTweet(tweet);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) => null);
+  }
 
   void _shareTextTweet({
     required String text,
